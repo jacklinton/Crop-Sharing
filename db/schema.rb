36b4c683b278b42ddd62f0f7a342e63fddfab9a3
Jack Linton <jacklinton@mac.com>
2017-03-13 18:56:34 +0000

    Added photo attachment column to events table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313180257) do
+ActiveRecord::Schema.define(version: 20170313183227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,12 @@ ActiveRecord::Schema.define(version: 20170313180257) do
     t.float    "lat"
     t.float    "lng"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "invites", force: :cascade do |t|
