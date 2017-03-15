d49cf0c14b9dc42210c52363c2d2b09e6f98177f
Jack Linton <jacklinton@mac.com>
2017-03-15 01:03:09 +0000

    Added user_id column to items table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314195018) do
+ActiveRecord::Schema.define(version: 20170315004950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170314195018) do
     t.boolean  "bring"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "listings", force: :cascade do |t|
