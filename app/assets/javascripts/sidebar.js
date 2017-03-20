2e1a60a32daab8505a82922af37581edad55431b
Jack Linton <jacklinton@mac.com>
2017-03-20 12:35:00 +0000

    Starting moving sidbar code into views pages.

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,4 +1,6 @@
-$("#menu-toggle").click(function(e) {
+$(document).ready(function() {
+    $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+});
